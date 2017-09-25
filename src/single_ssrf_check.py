8269e5e14757b570184e52fb49f66c199d5985ce
Lucas <espositolucas95@gmail.com>
2017-09-25 15:56:18 -0300

    pep8

--- src/single_ssrf_check.py
+++ src/single_ssrf_check.py
@@ -8,6 +8,7 @@ functions = [requests.get, requests.head, requests.options, requests.post, reque
 def request_function_to_string(function):
     return str(function).split(' ')[1].upper()
 
+
 class SSRFVulnerability:
 
     def __init__(self, target_url, payload, function):
@@ -20,7 +21,6 @@ def is_vulnerable(target_url, payload, function):
     # TODO: Validate connection exceptions
     response = function(target_url + payload)
     return response.ok and server_connector.succeed_connection_from(target_url, request_function_to_string(function))
-    
 
 
 def check_ssrf_for_site(target_url, payload):
